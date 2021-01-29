63ad28793a0ac36a3bb630bdd4bf15b50871fafa
Sakib12345 <sakibalislam12345@gmail.com>
2021-01-29 15:10:09 +0600

    case minus button done

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
 const plusBtn = document.getElementById('plusBtn').addEventListener(
     'click', ()=>{
-        handleProductChange(true);
+        handlePhoneChange(true);
     }
 )
 
 //minus button handler
 const minusBtn = document.getElementById('minusBtn').addEventListener(
     'click', ()=>{
-        handleProductChange(false);
+        handlePhoneChange(false);
     }
 )
 
-function handleProductChange(isIncrease) {
-    const caseInput = document.getElementById('case-count');
+
+const caseIncrease = document.getElementById('case-increase').addEventListener(
+    'click', ()=>{
+        const caseInput = document.getElementById('case-count');
         const caseCount = parseInt(caseInput.value);
-        let caseNewCount = caseCount;
-        if (isIncrease == true) {
-            caseNewCount = caseCount + 1;
-        }
-        if (isIncrease == false && caseCount > 0) {
-            caseNewCount = caseCount - 1;
-        }
+        const caseNewCount = caseCount + 1;
         caseInput.value = caseNewCount;
-        const caseTotal = caseNewCount * 1219;
-        document.getElementById('caseTotal').innerText = '$' + caseTotal;
+    }
+)
+
+
+
+
+
+
+function handlePhoneChange(isIncrease) {
+    const phoneInput = document.getElementById('phone-count');
+        const phoneCount = parseInt(phoneInput.value);
+        let phoneNewCount = phoneCount;
+        if (isIncrease == true) {
+            phoneNewCount = phoneCount + 1;
+        }
+        if (isIncrease == false && phoneCount > 0) {
+            phoneNewCount = phoneCount - 1;
+        }
+        phoneInput.value = phoneNewCount;
+        const phoneTotal = phoneNewCount * 1219;
+        document.getElementById('phoneTotal').innerText = '$' + phoneTotal;
 }
+
 
 
 
